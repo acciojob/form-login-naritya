461b038ad28922e78cf0ffe4f4428ec10da7ecb5
naritya <140882526+naritya@users.noreply.github.com>
2026-07-18 18:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function getFormvalue() {
+function getFormValue(e) {
     //Write your code here
+	
+		e.preventDefault();
+		let fname= document.getElementById("firstname").value;
+		let lname= document.getElementById("lastname").value;
+		alert(`${fname} ${lname}`)
 
 }
